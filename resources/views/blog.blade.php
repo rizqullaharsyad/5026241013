f3132d274576ff0e236da4cb4fc7e4ba0de31b07
rizqullaharsyad <rizqullah.arsyad@gmail.com>
2026-04-26 16:35:46 +0700

    Pertemuan 7

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,24 @@
+!DOCTYPE html>
+<html>
+<head>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+	<title>Tutorial Laravel - www.malasngoding.com</title>
+</head>
+<body>
+<div class="container">
+	<h3>www.malasngoding.com</h3>
+	<p>Seri Tutorial Laravel Lengkap Dari Dasar</p>
+	<p>Ini adalah view blog. ada di route blog.</p>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('blog', function () {
+    return view('blog');
+});
+
+Route::get('dosen', [DosenController::class, 'index']);
+Route::get('biodata', [DosenController::class, 'biodata']);
