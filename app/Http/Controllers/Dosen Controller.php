5547a8f026e3b8a29f03cca8d715ebb7263e3ae4
rizqullaharsyad <rizqullah.arsyad@gmail.com>
2026-04-26 16:38:43 +0700

    Pertemuan 7

---
+++ app/Http/Controllers/Dosen Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+//package kuliah.pert3
+use Illuminate\Http\Request;
+//Import
+class DosenController extends Controller
+{
+    //
+public function index(){
+    return "<h1>Halo ini adalah method index, dalam controller DosenController. - www.malasngoding.com</h1>";
+}
+
+public function biodata(){
+    $nama = "Rizqullah Arsyad" ;
+    $pelajaran = ["Algoritma & Pemrograman", "Kalkulus", "Pemrograman Web"];
+    return view('biodata', ['nama' => $nama, 'matkul' => $pelajaran]);
+}
+}
