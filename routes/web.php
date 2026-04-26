b4535e11c6700909e533bd55423fa81d42027722
rizqullaharsyad <rizqullah.arsyad@gmail.com>
2026-04-26 17:28:29 +0700

    Tugas Pertemuan 7

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('welcome', function () {
     return view('welcome');
 });
 
@@ -12,3 +12,36 @@ Route::get('blog', function () {
 
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
+
+Route::get('pert1', function () {
+    return view('pertemuan1');
+});
+
+Route::get('pert2', function () {
+    return view('pertemuan2');
+});
+
+Route::get('pert3', function () {
+    return view('pertemuan3');
+});
+
+Route::get('pert3tugas', function () {
+    return view('pertemuan3tugas');
+});
+
+Route::get('pert4', function () {
+    return view('pertemuan4');
+});
+
+Route::get('pert5', function () {
+    return view('pertemuan5');
+});
+
+Route::get('pert5index', function () {
+    return view('pertemuan5index'); // Contoh jika ingin lari ke index pertemuan 5
+});
+
+Route::get('pert5tugas', function () {
+    return view('pertemuan5tugas'); // Contoh jika ingin lari ke index pertemuan 5
+});
+
